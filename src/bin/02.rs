2c5f758eeb643c10a8a25c5176572bed3139ca31
Joe Ingledew <joe.ingledew@ofqual.gov.uk>
2025-12-02 10:23:25 +0000

    Day 2 pt 1

---
+++ src/bin/02.rs
@@ -0,0 +1,60 @@
+advent_of_code::solution!(2);
+
+// some slightly wobbly unwraps to be tidied up / handled properly
+fn parse_range(s: &str) -> Vec<u64> {
+    let (start, end) = s
+        .split_once("-")
+        .unwrap();
+    let start_n = start.parse::<u64>().unwrap();
+    let end_n = end.parse::<u64>().unwrap();
+    let mut res = Vec::new();
+    for i in start_n..(end_n+1) {
+        res.push(i);
+    }
+    res
+}
+
+fn is_id_invalid(id_num: &&u64) -> bool {
+    let id = id_num.to_string();
+    let length = id.len();
+    if length % 2 == 1 {
+        false
+    } else {
+        let (a, b) = id.split_at(length / 2);
+        a == b
+    }
+}
+
+fn is_id_invalid_pt2(id: &&u64) -> bool {
+    todo!();
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let ranges = input.split(",").map(parse_range).collect::<Vec<Vec<u64>>>().concat();
+    let res = ranges
+        .iter()
+        .filter(is_id_invalid)
+        .sum();
+    Some(res)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(1227775554));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
